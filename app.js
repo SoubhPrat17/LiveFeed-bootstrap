db7ed23a61e8627c6361f916becfcf73ba26bb0d
sourav21kumar <sourav21kumar@gmail.com>
2021-10-04 13:10:08 +0530

    converted to nodejs application

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.set('view engine', 'ejs');
+
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.listen(3000,()=>{
+    console.log("listening at port 3000")
+})
