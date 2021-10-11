9532aab49dcf521bd55fd6c3bb3f608dfeea2be9
sourav21kumar <sourav21kumar@gmail.com>
2021-10-11 10:33:13 +0530

    custom environment variable included

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -34,7 +34,7 @@ const ErrorHandlers = (err)=>{
 const maxAge = 3*24*60*60;
 const createToken = (id)=>{
 
-    return jwt.sign({id},'LIVEFEED',{
+    return jwt.sign({id},process.env.JWT_SECRET_STRING,{
         expiresIn:maxAge,
     })
 }
